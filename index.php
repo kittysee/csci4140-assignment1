d75a5e4057c841d06be394c622ea63085e53b647
kittysee <47625293+kittysee@users.noreply.github.com>
2019-02-15 23:47:05 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
-<?php
+<html>
+<body>
 
-echo hello;
+<form action="welcome.php" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
 
-?>
+</body>
+</html>
 
 
