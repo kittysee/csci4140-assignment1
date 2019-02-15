ec7b6708ee9f6889770df859d4048bc2704e7a49
kittysee <47625293+kittysee@users.noreply.github.com>
2019-02-15 23:45:53 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,23 +4,4 @@ echo hello;
 
 ?>
 
-<html>
-<head>
-	<title>Login Page</title>
-</head>
 
-<body>
-	<form>
-		<p>
-			username:<input type="text" name="user" />
-		</p>
-		<p>
-			password:<input type="text" name="pass" />
-		</p>
-		<p>
-			<input type="submit" value="login" name="login" />
-		</p>
-	</form>
-
-</body>
-</html>
