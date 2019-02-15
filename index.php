c27b154e0178b9117a3a51527b71821ba20cab80
kittysee <47625293+kittysee@users.noreply.github.com>
2019-02-15 23:45:14 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,6 @@
 <?php
 
-&db = parse_url(getenv("DATABASE-URL"));
-
-$pdo = new PDO("pgsql:" . sprintf("host=%s;port=%s;user=%s;password=%s;dbname=%s",
-	$db["host"],
-	$db["port"],
-	$db["user"],
-	$db["pass"],
-	ltrim($db["path"],"/")
-));
-
-if(isset($_POST['login'])){
-	session_start();
-	$username = $_POST['user'];
-	$password = $_POST['pass'];
+echo hello;
 
 ?>
 
